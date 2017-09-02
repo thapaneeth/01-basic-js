802b3aad59f21837e855ff5b976da860dff69451
thapaneeth <58160167@go.buu.ac.th>
2017-09-02 12:22:31 +0700

    Add files via upload

---
+++ tennis-game.js
@@ -0,0 +1,11 @@
+function TennisGame(){
+  const scoreString = ['Love', 'Fifteen', 'Thirty', 'forthy']
+  this.playerAGetScore = 0
+
+  this.reset = () =>{
+
+  }
+  this.echo=()=>{
+    return 'Love-Love'
+  }
+}
